e6bf3a2816587877bf04edea4c3fb986ea3f35e8
Musa AbdulHameed Lawal <lekzy.csharp@gmail.com>
2023-06-09 11:15:40 +0100

    refactor:refactor the fieldinfo by removing the clubs prev played

--- models/player.go
+++ models/player.go
@@ -24,12 +24,15 @@ type Address struct {
 
 type FieldInfo struct {
 	gorm.Model
-	PersonalInfoId          string   `json:"personal_info_id" gorm:"personal_info_id"`
-	YearOfExperience        string   `json:"year_of_experience" gorm:"year_of_experience"`
-	NumberOfGoalsScored     int      `json:"number_of_goals_scored" gorm:"number_of_goals_scored"`
-	ClubsHePreviouslyPlayed []string `json:"clubs_he_previously_played" gorm:"clubs_he_previously_played"`
-	JerseyNumber            int      `json:"jersey_number" gorm:"jersey_number"`
-	YearJoined              string   `json:"year_joined" gorm:"year_joined"`
-	PositionOnTheField      string   `json:"position_on_the_field" gorm:"position_on_the_field"`
-	PlayerClubStatus        string   `json:"player_club_status" gorm:"player_club_status"`
+	PersonalInfoId      string `json:"personal_info_id" gorm:"personal_info_id"`
+	YearOfExperience    string `json:"year_of_experience" gorm:"year_of_experience"`
+	NumberOfGoalsScored int    `json:"number_of_goals_scored" gorm:"number_of_goals_scored"`
+	JerseyNumber        int    `json:"jersey_number" gorm:"jersey_number"`
+	YearJoined          string `json:"year_joined" gorm:"year_joined"`
+	PositionOnTheField  string `json:"position_on_the_field" gorm:"position_on_the_field"`
+	PlayerClubStatus    string `json:"player_club_status" gorm:"player_club_status"`
 }
+
+/*implement this
+ClubsHePreviouslyPlayed []string `json:"clubs_he_previously_played" gorm:"clubs_he_previously_played"`
+*/
