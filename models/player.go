8625cf3a68299436d4bc844b88a7d3c51d859eaa
Musa AbdulHameed Lawal <lekzy.csharp@gmail.com>
2023-06-11 16:18:18 +0100

    refactor: refactor the models

--- models/player.go
+++ models/player.go
@@ -4,33 +4,40 @@ import "gorm.io/gorm"
 
 type PersonalInfo struct {
 	gorm.Model
-	Id            string  `json:"id" bson:"id"`
-	FirstName     string  `json:"first_name" bson:"first_name"`
-	LastName      string  `json:"last_name" bson:"last_name"`
-	Gender        string  `json:"gender" bson:"gender"`
-	MaritalStatus string  `json:"marital_status" bson:"marital_status"`
-	Email         string  `json:"email" bson:"email"`
-	PhoneNumber   string  `json:"phone_number" bson:"phone_number"`
-	Address       Address `json:"address" bson:"address"`
+	Id            string  `json:"id" gorm:"id"`
+	FirstName     string  `json:"first_name" gorm:"first_name"`
+	LastName      string  `json:"last_name" gorm:"last_name"`
+	Gender        string  `json:"gender" gorm:"gender"`
+	MaritalStatus string  `json:"marital_status" gorm:"marital_status"`
+	Email         string  `json:"email" gorm:"email"`
+	PhoneNumber   string  `json:"phone_number" gorm:"phone_number"`
+	Address       Address `json:"address" gorm:"address"`
 }
 
 type Address struct {
 	gorm.Model
 	PersonalInfoId string `json:"personal_info_id" gorm:"personal_info_id"`
-	Name           string `json:"name" bson:"name"`
-	ZipCode        string `json:"zip_code" bson:"zip_code"`
-	City           string `json:"city" bson:"city"`
+	Name           string `json:"name" gorm:"name"`
+	ZipCode        string `json:"zip_code" gorm:"zip_code"`
+	City           string `json:"city" gorm:"city"`
 }
 
 type FieldInfo struct {
 	gorm.Model
-	Id                      string   `json:"id" bson:"id"`
-	PersonalInfoId          string   `json:"player_ifo_id" bson:"player_ifo_id"`
-	YearOfExperience        string   `json:"year_of_experience" bson:"year_of_experience"`
-	NumberOfGoalsScored     int      `json:"number_of_goals_scored" bson:"number_of_goals_scored"`
-	ClubsHePreviouslyPlayed []string `json:"clubs_he_previously_played" bson:"clubs_he_previously_played"`
-	JerseyNumber            int      `json:"jersey_number" bson:"jersey_number"`
-	YearJoined              string   `json:"year_joined" bson:"year_joined"`
-	PositionOnTheField      string   `json:"position_on_the_field" bson:"position_on_the_field"`
-	PlayerClubStatus        string   `json:"player_club_status" bson:"player_club_status"`
+	Id                  string `json:"id" gorm:"id"`
+	PersonalInfoId      string `json:"player_ifo_id" gorm:"player_ifo_id"`
+	YearOfExperience    string `json:"year_of_experience" gorm:"year_of_experience"`
+	NumberOfGoalsScored int    `json:"number_of_goals_scored" gorm:"number_of_goals_scored"`
+	JerseyNumber        int    `json:"jersey_number" gorm:"jersey_number"`
+	YearJoined          string `json:"year_joined" gorm:"year_joined"`
+	PositionOnTheField  string `json:"position_on_the_field" gorm:"position_on_the_field"`
+	PlayerClubStatus    string `json:"player_club_status" gorm:"player_club_status"`
+}
+
+type ClubsHePreviouslyPlayed struct {
+	gorm.Model
+	PersonalInfoId string `json:"personal_info_id" gorm:"personal_info_id"`
+	ClubName       string `json:"club_name", gorm:"club_name"`
+	StartedYear    string `json:"started_year" gorm:"started_year"`
+	EndedYear      string `json:"ended_year" gorm:"ended_year"`
 }
