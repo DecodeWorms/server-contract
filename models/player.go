b19fcd6f4683c5c1ab306c2f401237e55d83f522
Musa AbdulHameed Lawal <lekzy.csharp@gmail.com>
2023-06-11 16:04:29 +0100

    refactor:refactored models

--- models/player.go
+++ models/player.go
@@ -16,16 +16,16 @@ type PersonalInfo struct {
 
 type Address struct {
 	gorm.Model
-	PlayerInfoId string `json:"personal_info_id" gorm:"personal_info_id"`
-	Name         string `json:"name" bson:"name"`
-	ZipCode      string `json:"zip_code" bson:"zip_code"`
-	City         string `json:"city" bson:"city"`
+	PersonalInfoId string `json:"personal_info_id" gorm:"personal_info_id"`
+	Name           string `json:"name" bson:"name"`
+	ZipCode        string `json:"zip_code" bson:"zip_code"`
+	City           string `json:"city" bson:"city"`
 }
 
 type FieldInfo struct {
 	gorm.Model
 	Id                      string   `json:"id" bson:"id"`
-	PlayerInfoId            string   `json:"player_ifo_id" bson:"player_ifo_id"`
+	PersonalInfoId          string   `json:"player_ifo_id" bson:"player_ifo_id"`
 	YearOfExperience        string   `json:"year_of_experience" bson:"year_of_experience"`
 	NumberOfGoalsScored     int      `json:"number_of_goals_scored" bson:"number_of_goals_scored"`
 	ClubsHePreviouslyPlayed []string `json:"clubs_he_previously_played" bson:"clubs_he_previously_played"`
